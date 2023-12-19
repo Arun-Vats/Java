ebd588b1d3555d3f047bc87b856d2cb13ee88ea4
Arun-Vats <asvats374@gmail.com>
2023-12-19 13:55:42 +0530

    operators

---
+++ arthmeticoperators.java
@@ -0,0 +1,15 @@
+public class arthmeticoperators {
+    public static void main(String[] args) {
+        int a,b,sum,sub,mul,div,rem;
+        a=1000;
+        b=10;
+        sum=a+b;
+        sub=a-b;
+        mul=a*b;
+        div=a/b;
+        rem=a%b;
+    
+        System.out.println(sum+" "+sub+" "+mul+" "+div+" "+rem);
+
+    }
+}

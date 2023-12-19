877edb25af7f83e595c3c75a5107e0ef4e8a2150
Arun-Vats <asvats374@gmail.com>
2023-12-19 10:32:38 +0530

    Variables.

---
+++ Variables.java
@@ -0,0 +1,7 @@
+public class Variables{
+    public static void main(String[] args) {
+        String Name = "Arun";
+        int age = 21;
+        System.out.println(Name+"'s age is "+age);
+    }
+}

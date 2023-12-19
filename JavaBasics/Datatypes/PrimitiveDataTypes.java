d86b50331bb8052ceee55324bcf7cf0b1b061e28
Arun-Vats <asvats374@gmail.com>
2023-12-19 10:59:46 +0530

    Primitive Data Type.

---
+++ JavaBasics/Datatypes/PrimitiveDataTypes.java
@@ -0,0 +1,18 @@
+package JavaBasics.Datatypes;
+
+public class PrimitiveDataTypes {
+  private static final boolean True = false;
+
+  public static void main(String[] args) {
+    
+  byte b = 127; //can store values from -128 to 127
+  short s = 1234; // 2 byte
+  int i = 264732342; // 4 byte
+  long l = 7327562376786L; // 8 byte
+  float f = 271547.44345F;  //4 byte
+  double d = 3274678.436576;  //8 byte
+  char ch = 'A';  //2 byte
+  boolean bb = true; // 1 byte
+  System.out.println(bb);
+}
+}

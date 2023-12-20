b98a8dff57a5bd3a96e53b80befc6ee4530fce57
Arun-Vats <asvats374@gmail.com>
2023-12-20 12:58:53 +0530

    math class.

---
+++ JavaBasics/mathclass.java
@@ -0,0 +1,7 @@
+public class mathclass {
+    public static void main(String[] args) {
+        System.out.println(Math.max(33,2));  //Math.max returns largest number
+        System.out.println(Math.min(4,3));   //Math.max returns smallest number
+        System.out.println((int)(Math.random()*100));
+    }
+}

0d3f94c282069cbebccc3c745ae086bea0a7f9e0
Arun-Vats <asvats374@gmail.com>
2023-12-20 13:37:21 +0530

    program to input a line

---
+++ JavaBasics/inputline.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class inputline {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is Your Name:-");
+        String Name = sc.nextLine();
+        System.out.println(Name);
+    }
+}

816e894f121ada16d202659d4da169aeead76f45
Arun-Vats <asvats374@gmail.com>
2023-12-20 14:48:38 +0530

    relational operators.

---
+++ JavaBasics/Operators/equalityinequality.java
@@ -0,0 +1,15 @@
+package Operators;
+public class equalityinequality {
+    public static void main(String[] args) {
+        // These are the different opeprators that we use in java 
+        // 1. equal to (==) :- this is used to return true if the given condition is eual
+        // 2. Not Equal to (!=) :- This is used to return true if the given confition is not equal
+        
+            System.out.println(10==10); //returns true
+            System.out.println(10==9); // returns false
+            System.out.println(10!=10); //returns false
+            System.out.println(10!=9);  //returns true
+
+            
+    }
+}
---
+++ JavaBasics/Operators/relationaloperator.java
@@ -0,0 +1,25 @@
+package Operators;
+
+public class relationaloperator {
+    public static void main(String[] args) {
+        
+//    1. less than (<)
+//    2. greater than (>)
+//    3. less than or equal to (<=)
+//    4. greater than or equal to (>=)
+
+//these are some of examples that returns true 
+System.out.println(10<11);
+System.out.println(10>9);
+System.out.println(29<=29);
+System.out.println(23>=23);
+
+//these are some examples that will print false
+System.out.println(11<10);
+System.out.println(9>10);
+System.out.println(30<=29);
+System.out.println(20>=23);
+
+
+}
+}

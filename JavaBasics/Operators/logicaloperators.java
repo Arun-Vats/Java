265a269416c6a33156155ae0447842b55641d7d9
Arun-Vats <asvats374@gmail.com>
2023-12-21 13:53:54 +0530

    logical operator

---
+++ JavaBasics/Operators/logicaloperators.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class logicaloperators {
+    public static void main(String[] args) {
+        // there are three types of operators in logical operators
+        // 1. logical and (&&)
+        // 2. logical or (||)
+        // 3. logical not (!)
+        System.out.println((10<=10)&&(22>20));
+        System.out.println((2>5)||(2<5));
+        System.out.println(!(2>5));
+        System.out.println((10<12)&&(10>44));
+        System.out.println((10>20)||(2>5));
+
+    }
+}

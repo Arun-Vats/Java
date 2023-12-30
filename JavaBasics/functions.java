e2d68f7347d7c8f0035c2b67c03199a4a664332a
Arun-Vats <asvats374@gmail.com>
2023-12-30 12:05:18 +0530

    functions program

---
+++ JavaBasics/functions.java
@@ -0,0 +1,19 @@
+public class functions {
+
+// Function to take input and print sum of two numbers.
+    public static void printsum(int a, int b){
+        int sum=a+b;
+        System.out.println(sum);
+    }
+
+
+    //Function to take two int inputs and print multiplication
+    public static void printmulti(int aa,int bb){
+        int mul=aa*bb;
+        System.out.println(mul);
+    }
+    public static void main(String[] args) {
+        printsum(20, 11);
+        printmulti(20,10);
+    }
+}

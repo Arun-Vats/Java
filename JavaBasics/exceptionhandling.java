1bb0af61213415e9f55f74f76c1bee9e6db58568
Arun-Vats <asvats374@gmail.com>
2023-12-30 11:47:28 +0530

    used try and catch block to avoid exceptions.

---
+++ JavaBasics/exceptionhandling.java
@@ -0,0 +1,12 @@
+public class exceptionhandling {
+    public static void main(String[] args) {
+        int a[]={12,15,14};
+        try{
+System.out.println(a[4]);
+        }
+        catch(Exception exception){
+//do something with the exception
+        }
+        System.out.println("My Name is Arun.");
+    }
+}
